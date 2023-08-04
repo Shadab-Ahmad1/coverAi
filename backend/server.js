82e03d6581c8bfeb8cbff224defd1a9c074b026d
shadab <ashadab124@gmail.com>
2023-08-04 23:31:09 +0500

    save user info in database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 require("dotenv").config()
 const express = require('express')
 const cors = require('cors') 
+const mongoose = require ('mongoose')
 const app = express()
 app.use(express.json())
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
+
+// Connect to your MongoDB database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(res =>{
+  console.log('connection successfull');
+});
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -11,11 +20,22 @@ const storeItems = new Map([
   [1,{priceInCents: 1000, name:"CoverLetter"}]
 ])
 
+// Define a mongoose schema for payments
+const paymentSchema = new mongoose.Schema({
+  email: String,
+  amount: Number,
+  timestamp: Date,
+});
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+
 app.post('/create-checkout-session', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
+      customer_email: req.body.email, 
       line_items: req.body.items.map(item => {
         const storeItem = storeItems.get(item.id);
         return {
@@ -26,12 +46,22 @@ app.post('/create-checkout-session', async (req, res) => {
             },
             unit_amount_decimal: storeItem.priceInCents,
           },
-          quantity: 1, // Set the quantity of the item
+          quantity: 1,
         };
       }),
-      success_url: `${process.env.SERVER_URL}/success`,
+      success_url: `${process.env.SERVER_URL}/thank-you`,
       cancel_url: `${process.env.SERVER_URL}/cancel`,
     });
+    
+        // Create a new payment document in MongoDB
+        const newPayment = new Payment({
+          email: req.body.email,
+          amount: storeItems.get(req.body.items[0].id).priceInCents,
+          timestamp: new Date(),
+        });
+
+        await newPayment.save();
+    
     res.json({ url: session.url });
   } catch (error) {
     console.error(error);
