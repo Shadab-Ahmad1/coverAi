c7850c60ca641f65dcc7528459cea2e715a0352d
“[firstname <aliimran98887@gmail.com>
2023-08-17 21:20:03 +0500

    after dashboard UI

--- front-end/src/components/client/dashboard.js
+++ front-end/src/components/client/dashboard.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../../assets/logo.png";
 import img from "../../assets/icon-cover.png";
 import './dashboard.css';
-import logoutImg from "../../assets/logout.png";
+import Register from '../Register';
+
 
 function Dashboard() {
+  const [showRegister, setShowRegister] = useState(false); // State to manage whether to show Register component
+
+  // Function to handle clicking on "Create New Cover Letter" button
+  const handleCreateNewCoverLetter = () => {
+    setShowRegister(true);
+  };
+
+
   return (
     <>
     <div className='dashboard-whole-container'>
@@ -15,39 +25,41 @@ function Dashboard() {
            <h6 className='dashboard-email'>aliimran98887@gmail.com</h6>
         </div>
         <div className='left-dashboard-second-container'>
-        <a className='left-dashboard-second-container-button'>
+        <a className='left-dashboard-second-container-button' onClick={handleCreateNewCoverLetter}>
         <span className='circle'>
             <i className='fa fa-plus'></i>
         </span>
           New cover Letter
         </a> 
         </div>
-        <br></br>
-        <br></br>
-        <hr className='line'/>
         <div className='left-dashboard-3-container'>
-          <a className='left-dashboard-3-container-button'>
+          <a className='left-dashboard-3-container-button' href='/client/dashboard'>
             <span className='second-span'>
             <i> <img className='second-img' src={img} /></i>
             </span>
             Cover Letter
           </a>
         </div>
-        <br></br>
-        <br></br>
-        <hr className='line'/>
         <div className='left-dashboard-4-container'>
           <a className='left-dashboard-4-container-button'>
-            <span >
-              <img  className='third-img' src= {logoutImg} alt='no img' />
-            </span>
-            Logout
+            <div className='logout-img'></div>
+           <lable className='label-class'>Logout</lable> 
           </a>
         </div>
        </div>
        <div className='right-dashboard-container'>
        <div className='right-dashboard-second-container'>
-       
+       {showRegister ? (
+              <Register />
+            ) : (
+       <div className='right-card-container'>
+        <p className='p-box empty'>
+        Looks like you haven't created a cover letter yet ! Please click on  
+        <strong> New Cover Letter </strong>
+        button to create one.
+        </p>
+       </div>
+            )}
        </div>
        
       </div>
